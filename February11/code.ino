76c2ab04a81e97ee95bd0ea9c8945dca90a8057e
HeonYeong(Andy) Lee <54748993+leeheonyeong@users.noreply.github.com>
2020-02-11 16:29:21 +0400

    Update code.ino

--- February11/code.ino
+++ February11/code.ino
@@ -1,25 +1,24 @@
-const int pResistor = A0; 
-const int ledPin=10;       
+const int pResistor = A0;
+const int ledPin = 10;
 
-int value;          
+int value;
 
-void setup(){
- pinMode(ledPin, OUTPUT);  
- pinMode(pResistor, INPUT);
- Serial.begin(9600);
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(pResistor, INPUT);
+  Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
   value = analogRead(pResistor);
   Serial.println(value);
   //You can change value "25"
-  if (value > 600){
+  if (value > 600) {
     digitalWrite(ledPin, LOW);  //Turn led off
   }
-  else{
+  else {
     digitalWrite(ledPin, HIGH); //Turn led on
   }
 
-  delay(50); 
+  delay(50);
 }
-
